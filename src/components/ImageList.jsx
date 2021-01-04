c3ce0bf45e97189a1d4c877353e26ec7dc1b3c9a
Nabeel Ahmad Khan <nabeel.ahmed@odinesolutions.com>
2021-01-04 21:53:59 +0500

    Initial Commit

---
+++ src/components/ImageList.jsx
@@ -0,0 +1,15 @@
+import '../assets/styles/ImageList.css';
+import React from 'react';
+
+import ImageCard from './ImageCard';
+
+const ImageList = (props) => {
+
+    const ImageList = props.images.map((image) => {
+        return <ImageCard key={image.id} image={image} />
+    });
+
+    return <div className="image-list">{ImageList}</div>;
+}
+
+export default ImageList;
